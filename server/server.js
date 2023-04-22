a9a07c765ea4479d793a26029e270aed5da48b4b
Pavel_eth <ethpavel@gmail.com>
2023-04-23 00:02:23 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const clients = {};
+
+app.use(express.static('public'));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/stream/:id', (req, res) => {
+  const id = req.params.id;
+  if (!clients[id]) {
+    res.status(404).send('Stream not found');
+  } else {
+    res.sendFile(__dirname + '/public/viewer.html');
+  }
+});
+
+wss.on('connection', (ws) => {
+  const id = generateUniqueId();
+  clients[id] = ws;
+  console.log(`New streamer connected with id ${id}`);
+
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log(`Streamer with id ${id} disconnected`);
+    delete clients[id];
+  });
+});
+
+function generateUniqueId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ streamer/streamer.js
@@ -0,0 +1,55 @@
+const WebSocket = require('ws');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const ws = new WebSocket('ws://localhost:3000');
+
+ws.on('open', () => {
+    console.log('Connected to server');
+
+    const chunks = [];
+    const videoStream = ffmpeg()
+        .input('video=HDR webcam') // Replace with your USB webcam's name
+        .inputFormat('dshow')
+        .videoCodec('libx264')
+        .format('flv') // Make sure this is set to 'flv'
+        .size('1280x720') // Set resolution to 720p
+        .fps(30) // Set the framerate to 30
+        .outputOptions([
+            '-preset ultrafast',
+            '-tune zerolatency',
+            '-pix_fmt yuv420p',
+            '-g 60', // Keyframe interval
+            '-b:v 2000k', // Video bitrate
+        ])
+        .on('error', (err) => console.error(err))
+        .on('data', (chunk) => {
+            chunks.push(chunk);
+        }).pipe();
+
+        setInterval(() => {
+            if (chunks.length > 0) {
+              const data = Buffer.concat(chunks);
+              chunks.length = 0;
+              ws.send(data, { binary: true }, (err) => {
+                if (err) console.error('Error sending data:', err);
+              });
+            }
+          }, 50); 
+
+    videoStream.on('data', (data) => {
+        ws.send(data, { binary: true }, (err) => {
+        if (err) console.error('Error sending data:', err);
+        });
+    });
+});
+
+ws.on('error', (err) => {
+  console.error('WebSocket error:', err);
+});
+
+ws.on('close', () => {
+  console.log('Disconnected from server');
+});
